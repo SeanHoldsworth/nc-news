7f493810c9e860413bf6b7155358c6a3d7ec2dda
Sean Holdsworth <sh@hob.cx>
2024-05-06 14:43:09 +0100

    Added CORS

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
 const apiRouter = require('./routes/api.router');
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api', apiRouter);
 
